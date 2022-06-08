ad4301d279d4f87edbf54d62a3da14432f62156a
jiseongTak <jeff3877@gmail.com>
2022-06-09 01:49:50 +0900

    PostsRepositoryTest

---
+++ src/test/java/com/example/springbootaws/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.example.springbootaws.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class PostsRepositoryTest {
+    
+    @Autowired PostsRepository postsRepository;
+    
+    @AfterEach  // 단위테스트가 끝날 때마다 수행
+    void cleanup() {
+        postsRepository.deleteAll();
+    }
+    
+    @Test
+    void 게시글저장_불러오기() throws Exception {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("jojoldu@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}

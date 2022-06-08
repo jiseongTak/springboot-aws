55b812072220e895807cd559577b6ab9965307ae
jiseongTak <jeff3877@gmail.com>
2022-06-09 00:17:30 +0900

    HellControllerTest

---
+++ src/test/java/com/example/springbootaws/web/HelloControllerTest.java
@@ -0,0 +1,31 @@
+package com.example.springbootaws.web;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("hello가 리턴된다")
+    void test() throws Exception {
+        String hello = "hello";
+        mvc.perform(get("/hello")) // get 요청
+                .andExpect(status().isOk()) // 200
+                .andExpect(content().string(hello));
+    }
+
+}

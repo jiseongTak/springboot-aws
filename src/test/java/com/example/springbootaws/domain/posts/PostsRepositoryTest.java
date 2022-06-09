7e2609caee0c424c8078c92ef65b42710421fa73
jiseongTak <jeff3877@gmail.com>
2022-06-10 00:52:15 +0900

    JPA Auditing 추가

--- src/test/java/com/example/springbootaws/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/example/springbootaws/domain/posts/PostsRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -38,5 +39,28 @@ class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2022, 6, 10, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println("createdDate=" + posts.getCreatedDate());
+        System.out.println("modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
